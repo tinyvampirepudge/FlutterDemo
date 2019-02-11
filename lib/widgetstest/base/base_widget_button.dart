dedadf291e0d53987f186c224eb8e5571aeb2092
wangjianzhou <tinytongtong@163.com>
2019-02-11 17:50:52 +0800

    按钮

--- lib/widgetstest/base/base_widget_button.dart
+++ lib/widgetstest/base/base_widget_button.dart
@@ -7,6 +7,70 @@ class ButtonWidgetTest extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("按钮"),
       ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("RaisedButton"),
+              onPressed: onRaisedButtonPressed,
+            ),
+            FlatButton(
+              child: Text("FlatButton"),
+              onPressed: onFlatButtonPressed,
+            ),
+            OutlineButton(
+              child: Text("OutlineButton"),
+              onPressed: onOutlineButtonPressed,
+            ),
+            IconButton(
+              icon: Icon(Icons.thumb_up),
+              onPressed: onIconButtonPressed,
+            ),
+            FlatButton(
+              child: Text("自定义FlatButton"),
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              splashColor: Colors.grey,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              onPressed: onCustomFlatButtonPressed,
+            ),
+            RaisedButton(
+              child: Text("自定义RaisedButton"),
+              color: Colors.blue,
+              highlightColor: Colors.blue[700],
+              splashColor: Colors.grey,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              onPressed: onCustomRaisedButtonPressed,
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void onRaisedButtonPressed() {
+    print('默认的RaisedButton按钮被点击了');
+  }
+
+  void onFlatButtonPressed() {
+    print('默认的FlatButton按钮被点击了');
+  }
+
+  void onOutlineButtonPressed() {
+    print('默认的OutlineButton按钮被点击了');
+  }
+
+  void onIconButtonPressed() {
+    print('默认的IconButton按钮被点击了');
+  }
+
+  void onCustomFlatButtonPressed() {
+    print('自定义的FlatButton按钮被点击了');
+  }
+
+  void onCustomRaisedButtonPressed() {
+    print('自定义的RaisedButton按钮被点击了');
   }
 }
