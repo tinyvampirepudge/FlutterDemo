79805ac1d75674da3d48fd5e269380b57c135770
wangjianzhou <tinytongtong@163.com>
2019-02-15 10:55:41 +0800

    主题换肤功能

--- lib/widgetstest/functional/functional_widgets.dart
+++ lib/widgetstest/functional/functional_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:dart_demo1/widgetstest/functional/functional_widget_inherited.dart';
+import 'package:dart_demo1/widgetstest/functional/functional_widget_theme.dart';
 import 'package:dart_demo1/widgetstest/functional/functional_widget_will_pop_scope.dart';
 import 'package:flutter/material.dart';
 
@@ -31,6 +32,17 @@ class FunctionalWidgets extends StatelessWidget {
                 Navigator.push(context,
                     new MaterialPageRoute(builder: (context) {
                   return new InheritedWidgetTest();
+                }));
+              },
+            ),
+            RaisedButton(
+              child: Text("路由换肤功能——Theme"),
+              padding: EdgeInsets.all(10.0),
+              onPressed: () {
+                // 导航到新路由
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) {
+                  return new ThemeWidgetTest();
                 }));
               },
             ),
