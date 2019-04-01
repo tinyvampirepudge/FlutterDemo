f09d0f2d4c2c901dca915a4dec03e89ec0277f21
wangjianzhou <tinytongtong@163.com>
2019-04-01 20:26:48 +0800

    minxin

---
+++ lib/darttest/class_mixin_interface_test.dart
@@ -0,0 +1,24 @@
+main() {
+  Musician musician = Musician();
+  musician.entertainMe();
+}
+
+class Musician with Musical {
+
+}
+
+mixin Musical {
+  bool canPlayPiano = false;
+  bool canCompose = false;
+  bool canConduct = false;
+
+  void entertainMe() {
+    if (canPlayPiano) {
+      print('Playing piano');
+    } else if (canConduct) {
+      print('Waving hands');
+    } else {
+      print('Humming to self');
+    }
+  }
+}
