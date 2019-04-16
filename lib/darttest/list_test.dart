c571c960ee295d27f0877eb3a62c27c96da35abf
wangjianzhou <tinytongtong@163.com>
2019-04-16 10:53:45 +0800

    list.forEach

--- lib/darttest/list_test.dart
+++ lib/darttest/list_test.dart
@@ -3,5 +3,12 @@ main() {
   // constantList 指向的是一个常量，我们不能给它添加元素（不能修改它）
 //  constantList[1] = 1;       // error
   // constantList 本身不是一个常量，所以它可以指向另一个对象
-  constantList = [4, 5, 6];     // it's fine
+  constantList = [4, 5, 6]; // it's fine
+
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  list.forEach((num) {
+    if (num % 2 == 0) {
+      print('num:$num');
+    }
+  });
 }
